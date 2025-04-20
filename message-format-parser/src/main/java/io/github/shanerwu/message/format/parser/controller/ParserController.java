e3e0a108ec57f01eac1d8e6a5418ee05e35226dd
shaner <yunwu0128@gmail.com>
2025-04-20 21:52:06 +0800

    Add default messages

--- message-format-parser/src/main/java/io/github/shanerwu/message/format/parser/controller/ParserController.java
+++ message-format-parser/src/main/java/io/github/shanerwu/message/format/parser/controller/ParserController.java
@@ -1,13 +1,9 @@
 package io.github.shanerwu.message.format.parser.controller;
 
-import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
-
-import io.github.shanerwu.message.format.parser.utils.MessageParser;
 import io.github.shanerwu.message.format.parser.message.AbstractMessageSupport;
 import io.github.shanerwu.message.format.parser.message.MessageData;
 import io.github.shanerwu.message.format.parser.utils.MessageClassScanner;
+import io.github.shanerwu.message.format.parser.utils.MessageParser;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -24,11 +20,18 @@ import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class ParserController {
 
     private TreeItem<MessageData> root;
+
+    private Map<String, String> defaultMessages = new HashMap<>();
 
     @FXML
     private ComboBox<String> messageTypes;
@@ -52,6 +55,21 @@ public class ParserController {
     public void initialize() {
         createMessageTypeComboBox();
         createTreeTableView();
+        initializeDefaultMessages();
+
+        messageDetails.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            handleMessageDetailsSelection(newValue);
+        });
+    }
+
+    private void handleMessageDetailsSelection(String detailName) {
+        if (detailName != null) {
+            if (defaultMessages.containsKey(detailName)) {
+                textArea.setText(defaultMessages.get(detailName));
+            } else {
+                textArea.clear();
+            }
+        }
     }
 
     @FXML
@@ -115,6 +133,27 @@ public class ParserController {
         treeTable.setEditable(true);
 
         messageColumn.setCellFactory(TextFieldTreeTableCell.forTreeTableColumn());
+    }
+
+    private void initializeDefaultMessages() {
+        defaultMessages.put("Sample101 測試電文 Request", "TESTCODE" + "20250420203000" + "00247" +
+                                                         "S" + "002" +
+                                                         "0530a219-9ef1-4eaa-96f4-59793dedda34" + "75c63a5c-5198-4b44-bbfb-b070d814382f" + "622ad2cf-14ab-429c-b6cf-d4101c8a2ff9" +
+                                                         "a6ef45ba-0901-4691-9c9c-dc2be8380d6c" + "ed1d1a92-6c60-45bc-97e1-d123c00441f8" + "f150e0ee-1306-42c8-bec8-8f7937efcf43");
+        defaultMessages.put("Sample101 測試電文 Response", "0001" + "20250420203000" + "00027" +
+                                                          "1" + "001");
+        defaultMessages.put("Sample201 測試電文",          "0001" + "20250420203000" + "0147" +
+                                                          "02" +
+                                                          "20250401" + "02" +
+                                                          "01" + "0530a219-9ef1-4eaa-96f4-59793dedda34" +
+                                                          "02" + "75c63a5c-5198-4b44-bbfb-b070d814382f" +
+                                                          "0" +
+                                                          "20250402" + "03" +
+                                                          "01" + "622ad2cf-14ab-429c-b6cf-d4101c8a2ff9" +
+                                                          "02" + "a6ef45ba-0901-4691-9c9c-dc2be8380d6c" +
+                                                          "03" + "ed1d1a92-6c60-45bc-97e1-d123c00441f8" +
+                                                          "1"
+        );
     }
 
     private void showErrorDialog(Exception e) {
